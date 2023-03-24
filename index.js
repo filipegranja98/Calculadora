4016e76eba260ca230e7f2ff09f19a848c209df4
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-03-24 10:36:17 -0300

    Criando estrutura da pagina
    Criei a estrutura inicial da página e comecei a trabalhar na parte do javascript.

---
+++ index.js
@@ -0,0 +1,23 @@
+    
+    const resultado = document.getElementById("resultado");
+    const zero = document.getElementById("zero");
+    const um = document.getElementById("um");
+    const dois = document.getElementById("dois");
+    const tres = document.getElementById("tres");
+    const quatro = document.getElementById("quatro");
+    const cinco = document.getElementById("cinco");
+    const seis = document.getElementById("seis");
+    const sete = document.getElementById("sete");
+        
+    const oito = document.getElementById("oito");
+    const nove = document.getElementById("nove");
+
+    const insert = (num) =>{
+            document.getElementById('resultado').innerHTML = num;
+    }
+     
+    if(sete.onclick){
+        insert(7);
+    }
+     
+
