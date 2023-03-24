90ab09bde62ea98adf47c8ac687b867f26a4eaa9
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-03-24 14:09:24 -0300

    Utilizando onclick
    Utilizei a função onclick() para capturar quais números o usuário esta clicando

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 
-    const zero = document.getElementById("zero");
-    const um = document.getElementById("um");
-    const dois = document.getElementById("dois");
-    const tres = document.getElementById("tres");
-    const quatro = document.getElementById("quatro");
-    const cinco = document.getElementById("cinco");
-    const seis = document.getElementById("seis");
-    const sete = document.querySelector("sete");
-    const oito = document.getElementById("oito");
-    const nove = document.getElementById("nove");
-
     const insert = (num) =>{
-            document.getElementById('resultado').innerHTML = num;
+       
+        const numero = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = numero + num; 
     }
-     
+
     
-     
+   
+
