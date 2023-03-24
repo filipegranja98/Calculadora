fdca3503fcefeb6d87b57e8240d611e56ab531a1
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-03-24 14:21:53 -0300

    Limpar e apagar
    Criei 2 funções uma para apagar todos os números digitados e outra para pagar somente um

--- index.js
+++ index.js
@@ -5,6 +5,14 @@
         document.getElementById('resultado').innerHTML = numero + num; 
     }
 
+    const limpar = () =>{
+        document.getElementById('resultado').innerHTML = "";
+    }
+
+    const apagar = () => {
+        const resultado = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1); 
+    }
     
    
 
